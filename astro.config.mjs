a9fccf906037abacb4c7f5e04077aeea83bab543
Xitari <79494839+XYZp11t@users.noreply.github.com>
2026-01-19 04:27:52 -0600

    Add base path to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://xyzp11t.github.io'
+  site: 'https://xyzp11t.github.io',
+  base:  '/'
 });
